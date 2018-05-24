418dbbad8b1c264b7ed5ef340d09914bb92983af
eisacke <emily@isacke.com>
2018-05-24 19:19:39 +0100

    New design 2018

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 $(()  => {
-  var colors = ['blue', 'lilac', 'pink'];
-  var $body = $('body');
-  var classToAdd = colors[Math.floor(Math.random() * colors.length)];
-  $body.addClass(classToAdd);
+  // var colors = ['blue', 'lilac', 'pink'];
+  // var $body = $('body');
+  // var classToAdd = colors[Math.floor(Math.random() * colors.length)];
+  // $body.addClass(classToAdd);
 });
