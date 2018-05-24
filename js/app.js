923f0af4fd218ebaf65da6b329528fddf4fb753b
eisacke <emily@isacke.com>
2018-05-24 20:29:46 +0100

    CV and removing JS

--- js/app.js
+++
@@ -1,6 +0,0 @@
-$(()  => {
-  // var colors = ['blue', 'lilac', 'pink'];
-  // var $body = $('body');
-  // var classToAdd = colors[Math.floor(Math.random() * colors.length)];
-  // $body.addClass(classToAdd);
-});
